c449cfbd4e73d17547d8525d2dfc834418261a5b
Selahattin Can Ölçer <95078118+canolcer@users.noreply.github.com>
2022-04-23 13:20:07 +0300

    Update Password.py

--- Password.py
+++ Password.py
@@ -9,11 +9,11 @@ total = numbers + punctuation + upper + lower
 
 
 password = ""
-length = random.randint(8, 64)
+length = random.randint(8, 64) # Creates length between 8-64
 i = 0
 while i < length:
     index = random.randint(0, len(total) - 1)
-    if not (total[index].lower() in password):
+    if not (total[index].lower() in password):   #over the length it uses string library in python to create a password
         password += total[index]
         i += 1
 print(password)
